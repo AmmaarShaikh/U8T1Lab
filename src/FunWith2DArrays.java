4936850600f1695a81381b88f5c985f13d516b90
BT_1E10_10 <eleanorc25@nycstudents.net>
2022-03-03 13:23:18 -0500

    day 2 changes

--- src/FunWith2DArrays.java
+++ src/FunWith2DArrays.java
@@ -10,4 +10,25 @@ public class FunWith2DArrays {
         System.out.println(lists[lists.length-1][0]);
         System.out.println(lists[lists.length-1][lists[lists.length-1].length-1]);
     }
+
+    public static double average(int[][] lists){
+        int total = 0;
+        for (int[] row : lists){
+            for (int col : row){
+                total += col;
+            }
+        }
+        return (double)total / (lists.length * lists[0].length);
+    }
+
+    public static int[] indexFound(String[][] lists, String target){
+        for (int row = 0; row < lists.length; row++){
+            for (int col = 0; col < lists[0].length; col++){
+                if (lists[row][col].equals(target)){
+                    return new int[]{row, col};
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
 }
